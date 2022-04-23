85811e696d651c0e325e786bb8c00202262ef48f
DEV-PROG-spec <devendra.srivastava_cs19@gla.ac.in>
2022-04-23 23:52:17 +0530

    Add Slider component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import Slider from "./components/Slider";
+import data from "./data/data.json";
 
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider start={data.banner.start} />
     </Router>
   );
 }
