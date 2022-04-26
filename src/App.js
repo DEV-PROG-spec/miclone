5cfeb7e2f21a2367a4993f5a1f1c08e87c27af97
DEV-PROG-spec <devendra.srivastava_cs19@gla.ac.in>
2022-04-26 17:03:55 +0530

    Add Components to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Slider from "./components/Slider";
 import data from "./data/data.json";
 import Offers from "./components/Offers";
 import Heading from "./components/Heading";
 import StarProducts from "./components/StarProducts";
+import HotaccessoriesMenu from "./components/HotaccessoriesMenu";
+import HotAccessories from "./components/HotAccessories";
 
 function App() {
   return (
@@ -18,6 +20,19 @@ function App() {
       <Heading text="STAR PRODUCTS" />
       <StarProducts />
       <Heading text="HOT ACCESSORIES" />
+      <HotaccessoriesMenu />
+      <Routes>
+        <Route
+          exact
+          path="/music"
+          element={
+            <HotAccessories
+              music={data.hotAccessories.music}
+              musicCover={data.hotAccessoriesCover.music}
+            />
+          }
+        />
+      </Routes>
     </Router>
   );
 }
